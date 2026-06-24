a29dc2b0aec65bd2e80590e771ff3de19b82d7b8
Robert Wadsworth <robert@robertwadsworth.dev>
2026-06-23 21:35:33 -0400

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.ts
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {ListNode}
+     */
+    reverseList(head: ListNode | null): ListNode {
+        let curr = head;
+        let prev = null;
+
+        while(curr) {
+            const next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            if (!next) {
+                return curr;
+            }
+            curr = next;
+            
+        }
+
+        return curr;
+    }
+}
