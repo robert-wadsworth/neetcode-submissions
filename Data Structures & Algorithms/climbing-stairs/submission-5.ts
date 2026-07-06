a9f0b3d0d7fae1fd13f51714aa5345b43b1601af
Robert Wadsworth <robert@robertwadsworth.dev>
2026-07-06 18:48:10 -0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n: number): number {
+        let one = 1;
+        let two = 1;
+
+        
+        
+        let i = n-2;
+        
+        while(i >= 0) {
+            const temp = one;
+            one = one + two;
+            two = temp;
+            i = i - 1;
+        }
+
+        return one;
+    }
+}
