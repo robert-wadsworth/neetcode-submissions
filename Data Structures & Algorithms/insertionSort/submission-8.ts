a88d8a5a1d041278589e4761890ebdb62201f526
Robert Wadsworth <robert@robertwadsworth.dev>
2026-07-09 09:33:45 -0400

    Add: insertionSort - submission-8

---
+++ Data Structures & Algorithms/insertionSort/submission-8.ts
@@ -0,0 +1,35 @@
+/**
+ * Pair class to store key-value pairs
+ */
+// class Pair {
+//     /**
+//      * @param {number} key The key to be stored in the pair
+//      * @param {string} value The value to be stored in the pair
+//      */
+//     constructor(key, value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    /**
+     * @param {Pair[]} pairs
+     * @returns {Pair[][]}
+     */
+    insertionSort(pairs: Pair[]): Pair[][] {
+        const result: Pair[][] = [];
+
+        for (let i: number = 0; i < pairs.length; i++) {
+            let j: number = i - 1;
+            while (j >= 0 && pairs[j + 1].key < pairs[j].key) {
+                const tmp: Pair = pairs[j];
+                pairs[j] = pairs[j + 1];
+                pairs[j + 1] = tmp;
+                j--;
+            }
+            result.push([...pairs]);
+        }
+
+        return result;
+    }
+}
