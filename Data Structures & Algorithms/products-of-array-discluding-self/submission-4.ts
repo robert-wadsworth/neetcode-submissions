73e0bb14c178765f2384ff64446c6078625d11f6
Robert Wadsworth <robert@robertwadsworth.dev>
2026-07-13 21:26:27 -0400

    Add: products-of-array-discluding-self - submission-4

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-4.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums: number[]): number[] {
+        const n: number = nums.length;
+        const result: number[] = new Array(n).fill(1);
+        
+        let prefix: number = 1;
+        for(let i: number = 0; i < n; i++) {
+            result[i] *= prefix;
+            prefix *= nums[i];
+        }
+
+        let suffix: number = 1;
+        for(let i: number = n - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+
+        return result;
+    }
+}
