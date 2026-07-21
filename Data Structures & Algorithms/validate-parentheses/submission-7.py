e4263e4296eb86ce180a93c58d551a7aa12b8b5e
Robert Wadsworth <robert@robertwadsworth.dev>
2026-07-20 21:03:57 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lookup = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c in lookup:
+                if stack and stack[-1] == lookup[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return len(stack) == 0
